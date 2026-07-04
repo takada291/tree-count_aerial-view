72b76b7aa54bac754267cccd8d5e8fea55110d32
takada291 <tkp291@gmail.com>
2026-07-04 10:48:46 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'treetop-ai-cache-v0.3.0'; // ★バージョンを変えることで超強力クリア発動
+const CACHE_NAME = 'treetop-ai-cache-v2.0.0'; // ★バージョンを変えることで超強力クリア発動
 const urlsToCache = [
   './',
   'index.html',
